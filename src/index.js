06429ab992ea98a27f7beb8e3eb9281d0e584205
Kihoon Han <khhan1993@gmail.com>
2017-04-27 20:11:03 +0900

    구조 변경 및 Order Request 부분 만드는 중

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import routes from './config/routes';
+import reducer from './reducer';
+
+import 'font-awesome/css/font-awesome.css';
+import 'semantic-ui-css/semantic.min.css';
+import '../public/style.css';
+
+let store = createStore(reducer);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    {routes}
+  </Provider>,
   document.getElementById('root')
 );
