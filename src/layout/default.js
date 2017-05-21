e50b752bca4f7a330f4ce9d415ae5dbbef921ced
zooomg <msgy95@gmail.com>
2017-05-21 21:57:27 +0900

    final

--- src/layout/default.js
+++ src/layout/default.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { Container, Menu, Dropdown, Header, Modal, Button, Grid, Icon, Segment} from 'semantic-ui-react';
-import { Glyphicon, Button, Navbar, Nav } from 'react-bootstrap';
+import { Glyphicon, Button, Navbar, Nav, NavItem } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import { Link, browserHistory } from 'react-router';
@@ -54,6 +54,10 @@ class Default extends React.Component {
     return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
   }
 
+  getCurrentRoute() {
+    return this.props.location.pathname.split("/");
+  }
+
   handleSignoutClick = (e) => {
     browserHistory.push("/main");
     this.props.signOut();
@@ -64,28 +68,63 @@ class Default extends React.Component {
       "is_mobile_menu_expanded": !this.state.is_mobile_menu_expanded
     });
   };
+  
+  handleFacebookLogin = () => {
+    window.FB.login((response) => {
+      if(response.status === 'connected') {
+        let url = this.props.api_url + "/api/user?type=facebook";
 
+
+        axios.post(url, response.authResponse)
+          .then((response) => {
+          this.props.signIn(response.data.jwt);
+          browserHistory.push("/group");
+        }).catch((error) => {
+          alert(error.response.data.message);
+        });
+      }
+    });
+  };
+
+  handleKakaoLogin = () => {
+    window.Kakao.Auth.login({
+      success: (authObj) => {
+        let access_token = authObj['access_token'];
+        let url = this.props.api_url + "/api/user?type=kakao";
+
+
+        axios.post(url, {
+          "accessToken": access_token
+        }).then((response) => {
+          this.props.signIn(response.data.jwt);
+          browserHistory.push("/group");
+          //window.Kakao.Auth.logout();
+        }).catch((error) => {
+          alert(error.response.data.message);
+        });
+      },
+      fail: (err) => {
+        console.log(err);
+      }
+    });
+  };
 
   render() {
     return (
       <div>
-      {this.props.jwt !== null && 
-      <div className="background_menu">
-      }
-      {this.props.jwt === null &&
-      <div className="background">
-      }  
-        <div className="container">
+      <div className={this.props.jwt === null ? "background" : "background_menu"}>
+        <div className="cover">
+	<div className="container">
           <Navbar>
-            <div className="container">
+	    <div className="container">
               <Navbar.Header>
                 <h2>HYUOMS</h2>
               </Navbar.Header>
-              <Navbar.Collapse>
+              <Navbar>
                 <Nav pullRight>
                   {this.props.jwt !== null &&
                   // <Dropdown item text="그룹" className={this.activeRoute("/group")}>
-                    <NavItem eventKey={1} href="/group" className={this.activeRoute("/group")}>GROUP</NavItem>
+                    <NavItem eventKey={1} href="/group">GROUP</NavItem>
                     
                       // <Link to="/group/list"><Dropdown.Item>그룹 목록</Dropdown.Item></Link>
                       // <Link to="/group/create"><Dropdown.Item>그룹 생성</Dropdown.Item></Link>
@@ -95,7 +134,7 @@ class Default extends React.Component {
 
                   {this.props.jwt !== null && this.props.group_id !== null &&
                   // <Dropdown item text='주문' className={this.activeRoute("/order")}>
-                    <NavItem eventKey={2} href="/order/request" className={this.activeRoute("/order")}>ORDER</NavItem>
+                    <NavItem eventKey={2} href="/order/request">ORDER</NavItem>
                     // <Dropdown.Menu>
                       // <Link to="/order/request"><Dropdown.Item>주문 입력</Dropdown.Item></Link>
                       // <Link to="/order/list"><Dropdown.Item>주문 내역</Dropdown.Item></Link>
@@ -107,17 +146,17 @@ class Default extends React.Component {
                   }
 
                   {this.props.jwt !== null && this.props.group_id !== null &&
-                  <NavItem eventKey={3} href="/queue" className={this.activeRoute("/queue")}>QUEUE</NavItem>
+                  <NavItem eventKey={3} href="/queue">QUEUE</NavItem>
                   // <Link to="/queue"><Menu.Item name="대기열" className={this.activeRoute("/queue")} /></Link>
                   }
 
                   {this.props.jwt !== null && this.props.group_id !== null &&
-                  <NavItem eventKey={4} href="#" className={this.activeRoute("/statistics")}>DASHBOARD</NavItem>
+                  <NavItem eventKey={4} href="/statistics">DASHBOARD</NavItem>
                   // <Menu.Item>통계 (준비중)</Menu.Item>
                   }
 
                   {this.props.jwt !== null && this.props.group_id !== null && this.props.role > 1 &&
-                  <NavItem eventKey={5} href="/manage/menu" className={this.activeRoute("/manage")}>SETTING</NavItem>
+                  <NavItem eventKey={5} href="/manage/menu">SETTING</NavItem>
                   // <Dropdown item text='관리' className={this.activeRoute("/manage")}>
                     // <Dropdown.Menu>
                       // <Link to="/manage/menu"><Dropdown.Item>메뉴 관리</Dropdown.Item></Link>
@@ -132,10 +171,10 @@ class Default extends React.Component {
                   // <Menu.Item name='로그아웃' onClick={this.handleSignoutClick} />
                   }
                 </Nav>
-              </Navbar.Collapse>
+              </Navbar>
             </div>
           </Navbar>
-          {this.props.jwt === null && 
+          {this.props.jwt === null &&
           <div className="masthead segment">
             <div className="container">
               <h1 className="text-center">휴:옴스</h1>
@@ -148,7 +187,8 @@ class Default extends React.Component {
               </h2>
             </div>
           </div>
-
+	  }
+	  {this.props.jwt === null &&
           <div className="footer">
             <div className="container">
               <br/>
@@ -157,11 +197,51 @@ class Default extends React.Component {
             </div>
           </div>
           }
-
+          {this.getCurrentRoute()[1]==='group' &&
+          <h2>그룹<br/><br/></h2>
+          }
+          {this.getCurrentRoute()[1]==='queue' &&
+          <h2>대기열<br/><br/></h2>
+          }
+          {this.getCurrentRoute()[1]==='statistics' &&
+          <h2>통계<br/><br/></h2>
+          }
+          {this.getCurrentRoute()[1]==='order' &&
+	  <div>
+          <h2>주문</h2>
+          <Nav activeKey={this.getCurrentRoute()[2]} className="nav-tabs">
+            <NavItem eventKey={"request"} href="/order/request">입력</NavItem>
+            <NavItem eventKey={"list"} href="/order/list">내역</NavItem>
+            <NavItem eventKey={"verify"} href="/order/verify">처리</NavItem>
+          </Nav>
+	  </div>
+          }
+          {this.getCurrentRoute()[1]==='manage' &&
+	  <div>
+          <h2>설정</h2>
+          <Nav activeKey={this.getCurrentRoute()[2]} className="nav-tabs">
+            <NavItem eventKey={"menu"} href="/manage/menu">메뉴</NavItem>
+            <NavItem eventKey={"setmenu"} href="/manage/setmenu">세트메뉴</NavItem>
+            <NavItem eventKey={"member_and_group"} href="/manage/member_and_group">그룹</NavItem>
+          </Nav>
+	  </div>
+          }
+  	  <Button href="/guide" bsStyle="link" bsSize="large" style={{float:'right'}}><Glyphicon glyph="question-sign"/></Button>
+	  </div>
+	</div>
+      </div>
+      <br/><br/>
+      {this.props.children}
+      {this.props.jwt !== null &&
+      <div className="footer">
+	<div className="container">
+  	  <br/><br/>
+  	  <p className="text-center" style={{color:'black'}}><Glyphicon glyph="copyright-mark" ></Glyphicon> 2014-2017. 한양대학교 <b>한기훈</b> All Rights Reserved.</p>
+          <br/>
         </div>
-        {this.props.children}
       </div>
-      </div>
+      }
+    </div>
     );
   }
 }
